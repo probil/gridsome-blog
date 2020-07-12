41b3fbcc3453f608f5e00446f9872a0559712994
Max Liashuk <max.lyashuk@rolique.io>
2020-07-12 15:30:48 +0300

    translate tag page

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto my-16">
-      <h2 class="text-4xl font-bold mb-8 border-b">Tag: {{ $page.tag.title }}</h2>
+      <h2 class="text-4xl font-bold mb-8 border-b">Мітка: {{ $page.tag.title }}</h2>
 
       <div v-for="post in $page.tag.belongsTo.edges" :key="post.node.id" class="post border-gray-400 border-b mb-12">
         <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.date }}</span>
           <span> &middot; </span>
-          <span>{{ post.node.timeToRead }} min read</span>
+          <span>{{ post.node.timeToRead }} хв читання</span>
         </div>
 
         <div class="text-lg mb-4">
@@ -16,7 +16,7 @@
         </div>
 
         <div class="mb-8">
-          <g-link :to="post.node.path" class="font-bold uppercase">Read More</g-link>
+          <g-link :to="post.node.path" class="font-bold uppercase">Продовжити читання</g-link>
         </div>
       </div>
 
@@ -46,7 +46,7 @@ query Tag ($id: ID!, $page: Int) {
           ...on Post {
             title
             timeToRead
-    	      date (format: "MMMM D, YYYY")
+    	      date (format: "D MMMM, Y", locale: "uk")
             path
             summary
             tags {
